b3279e0fb45a58897eb023585667693184bc490d
exgleowl <pnikolaev822@gmail.com>
2024-09-19 22:08:43 +0300

    add create message and get all messages routes

---
+++ app.js
@@ -0,0 +1,35 @@
+import bodyParser from 'body-parser'
+import express from 'express'
+import { query } from './db.js'
+
+const app = new express()
+
+app.use(bodyParser.json())
+
+app.post('/api/messages', async (req, res) => {
+  try {
+    const { username, text } = req.body
+    if (!username || !text || username.length < 2 || text.length < 1) {
+      return res.status(400).end()
+    }
+
+    const { rows } = await query(`INSERT INTO messages(username, text) VALUES($1, $2);`, [username, text])
+    const newMessage = rows[0]
+    res.status(201).json(newMessage)
+  } catch (err) {
+    res.status(500).end()
+  }
+})
+
+app.get('/api/messages', async (req, res) => {
+  try {
+    const { rows } = await query(`SELECT * FROM messages ORDER BY "createdAt" DESC;`)
+    const messages = rows
+    res.json({ messages })
+  } catch (err) {
+    res.status(500).end()
+  }
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => `app listening on port ${PORT}`)
